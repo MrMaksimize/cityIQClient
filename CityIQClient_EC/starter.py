59b91ef521ed878b0c7024b70c1746620744ba48
Erikcaldwell <39847875+Erikcaldwell@users.noreply.github.com>
2019-02-03 14:47:31 -0800

    updated with additional API calls
    Every event call is here except the location based calls.  Additionally, given video and image isn't available to anyone except law enforcement i haven't included those API calls.  I think this is a decent starter code.  More work on the descriptions / cleaning up and simplifying the code could be done.

--- CityIQClient_EC/starter.py
+++ CityIQClient_EC/starter.py
@@ -23,7 +23,9 @@ def get_token():
     print("Token Received")
     return token
 
-# returns a list of all requested assets based within the specified predix zone and bbox
+# returns a list of all requested assets based within the specified assetType, predix zone and bboxself.
+#assetTypes can be 'CAMERA', 'ENV_SENSOR', 'NODE'(parent asset), 'OTHERS' (list of non-standard devices)
+#predix zones are 'SD-IE-PARKING', 'SD-IE-ENVIRONMENTAL','SD-IE-PEDESTRIAN', 'SD-IE-TRAFFIC'
 def get_assets():
     assetType = "assetType:CAMERA"
     url = "https://ic-metadata-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/metadata/assets/search"
@@ -33,7 +35,7 @@ def get_assets():
     token = get_token()
     headers = {
         'Authorization': "Bearer {}".format(token),
-        'Predix-Zone-Id': "SD-IE-TRAFFIC",
+        'Predix-Zone-Id': "SD-IE-PARKING",
         'cache-control': "no-cache",
         'Postman-Token': "81281f86-66f2-461d-a33b-6e5794ac3b51"
         }
@@ -115,8 +117,8 @@ def get_traffic_event_by_bbox(lookback_minutes):
 lookback_minutes = 600
 
 #get current temperature by asset
-def get_current_temp():
-    asset = "92e6fb6e-f352-49ea-86f2-16d626f3d271"
+def get_temp():
+    asset = "0e6199d6-33b1-45d0-aa09-3dd51c04a77c"
     url_string = '/events'
     url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/assets/"+asset+url_string
     ts = datetime.datetime.now().timestamp()
@@ -138,4 +140,221 @@ def get_current_temp():
     response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
 
     print(response.text)
-get_current_temp()
+
+
+#get current pressure by asset
+def get_pressure():
+    asset = "0e6199d6-33b1-45d0-aa09-3dd51c04a77c"
+    url_string = '/events'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/assets/"+asset+url_string
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"PRESSURE","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+        'Predix-Zone-Id': "SD-IE-ENVIRONMENTAL",
+        'cache-control': "no-cache",
+        'Postman-Token': "79a9e5c2-c1dc-4dc3-bbe4-34c751fa389e"
+        }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+#get current orientation by asset
+def get_orientation():
+    asset = "0e6199d6-33b1-45d0-aa09-3dd51c04a77c"
+    url_string = '/events'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/assets/"+asset+url_string
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"ORIENTATION","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+        'Predix-Zone-Id': "SD-IE-ENVIRONMENTAL",
+        'cache-control': "no-cache",
+        'Postman-Token': "79a9e5c2-c1dc-4dc3-bbe4-34c751fa389e"
+        }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+#get current humidity by asset
+def get_humidity():
+    asset = "0e6199d6-33b1-45d0-aa09-3dd51c04a77c"
+    url_string = '/events'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/assets/"+asset+url_string
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"HUMIDITY","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+        'Predix-Zone-Id': "SD-IE-ENVIRONMENTAL",
+        'cache-control': "no-cache",
+        'Postman-Token': "79a9e5c2-c1dc-4dc3-bbe4-34c751fa389e"
+        }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+#get pedestrian count by asset
+def get_ped_by_asset():
+    asset = "0297e736-88e8-4a4a-8da0-be7f2dae52de"
+    url_string = '/events'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/assets/"+asset+url_string
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"PEDEVT","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+    'Predix-Zone-Id': "SD-IE-PEDESTRIAN",
+    'cache-control': "no-cache",
+    'Postman-Token': "944b1e16-6cfe-4970-ba30-5048e6b291e7"
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+
+#get pedestrian count by bbox
+def get_ped_by_bbox():
+    bbox = '32.7225326:-117.1674644,32.7176702:-117.1625865'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/locations/events"
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"PEDEVT","bbox":bbox,"locationType":"WALKWAY","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+    'Predix-Zone-Id': "SD-IE-PEDESTRIAN",
+    'cache-control': "no-cache",
+    'Postman-Token': "def220c5-80b6-4087-b81c-fee544d3077d"
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+#get parking IN count by asset
+def get_PKIN_by_asset():
+    asset = "027c652c-f764-422d-88d2-d6796b6eb17e"
+    url_string = '/events'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/assets/"+asset+url_string
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"PKIN","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+    'Predix-Zone-Id': "SD-IE-PARKING",
+    'cache-control': "no-cache",
+    'Postman-Token': "944b1e16-6cfe-4970-ba30-5048e6b291e7"
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+#get parking IN count by bbox
+def get_PKIN_by_bbox():
+    bbox = '32.7225326:-117.1674644,32.7176702:-117.1625865'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/locations/events"
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"PKIN","bbox":bbox,"locationType":"PARKING_ZONE","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+    'Predix-Zone-Id': "SD-IE-PARKING",
+    'cache-control': "no-cache",
+    'Postman-Token': "def220c5-80b6-4087-b81c-fee544d3077d"
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+
+#get parking OUT count by asset
+def get_PKOUT_by_asset():
+    asset = "027c652c-f764-422d-88d2-d6796b6eb17e"
+    url_string = '/events'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/assets/"+asset+url_string
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"PKOUT","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+    'Predix-Zone-Id': "SD-IE-PARKING",
+    'cache-control': "no-cache",
+    'Postman-Token': "944b1e16-6cfe-4970-ba30-5048e6b291e7"
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+#get parking OUT count by bbox
+def get_PKOUT_by_bbox():
+    bbox = '32.7225326:-117.1674644,32.7176702:-117.1625865'
+    url = "https://ic-event-service-sandiego.run.aws-usw02-pr.ice.predix.io/v2/locations/events"
+    ts = datetime.datetime.now().timestamp()
+    CityIQ_Current_TS = int(ts*1000)
+    CityIQ_TS_Calc_TS =  datetime.datetime.now() - timedelta(minutes=lookback_minutes)
+    CityIQ_Starttime_TS =  int((ts-(lookback_minutes*60))*1000)
+    querystring = {"eventType":"PKOUT","bbox":bbox,"locationType":"PARKING_ZONE","startTime":CityIQ_Starttime_TS,"endTime":CityIQ_Current_TS,"pageSize":"100"}
+
+    payload = ""
+    token = get_token()
+    headers = {
+        'Authorization': "Bearer {}".format(token),
+    'Predix-Zone-Id': "SD-IE-PARKING",
+    'cache-control': "no-cache",
+    'Postman-Token': "def220c5-80b6-4087-b81c-fee544d3077d"
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+
+    print(response.text)
+
+get_PKOUT_by_bbox()
